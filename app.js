69cdf77c6521cebcc73fabadcc6da068328e487b
Akiraaa07 <igorakira2004@gmail.com>
2024-10-04 21:43:50 -0300

    Configurando o CORS no backend

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const tarefaRoutes = require('./routes/tarefaRoutes');
 const userRoutes = require('./routes/userRoutes');
 const authMiddleware = require('./middlewares/authMiddleware');
 
+
 // Middleware para JSON
 app.use(express.json());
+
+// Inicializa o CORS
+app.use(cors()); 
 
 // Rotas
 app.use('/api', userRoutes); // Rota para autenticação
